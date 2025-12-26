43b9b00fbf2e98c54143e68f15fe750be840e0d2
Niraj-Sharma58 <niraj.sharma.engineer@gmail.com>
2025-12-26 22:54:02 +0545

    Edit Section

--- Controllers/ComputerPartsController.cs
+++ Controllers/ComputerPartsController.cs
@@ -44,5 +44,72 @@ namespace ComputerPart.Controllers
 
             return View(obj);
         }
+
+         // ================= EDIT (GET) =================
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var partFromDb = _context.ComputerParts.Find(id);
+
+            if (partFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(partFromDb);
+        }
+
+        // ================= EDIT (POST) =================
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(ComputerPartModel obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.ComputerParts.Update(obj);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        // ================= DELETE (GET) =================
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var partFromDb = _context.ComputerParts.Find(id);
+
+            if (partFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(partFromDb);
+        }
+
+        // ================= DELETE (POST) =================
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePOST(int id)
+        {
+            var obj = _context.ComputerParts.Find(id);
+
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            _context.ComputerParts.Remove(obj);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
